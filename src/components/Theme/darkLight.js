ff8c25e35ec06e93b037fa44076285bfba68c844
hervekarl <hervekarlmarx@gmail.com>
2025-05-21 15:52:11 +0100

    Correction dark et light

--- src/components/Theme/darkLight.js
+++ src/components/Theme/darkLight.js
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    // Essaie de récupérer la valeur en localStorage, sinon détecte la préférence système
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") 
+        || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+    }
+    return "light";
+  });
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
@@ -13,7 +21,7 @@ const ThemeToggle = () => {
 
   return (
     <button className="btn btn-primary" onClick={toggleTheme}>
-      Switch to {theme == "light" ? "Dark" : "Light"} Mode
+      Switch to {theme === "light" ? "Dark" : "Light"} Mode
     </button>
   );
 };
