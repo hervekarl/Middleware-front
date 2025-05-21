1363dc5a243233f4d4c47899c6aa6383a72937f7
hervekarl <hervekarlmarx@gmail.com>
2025-05-21 15:43:26 +0100

    Correction dark et light themes version 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // chemins à scanner pour les classes Tailwind
   ],
+  safelist: [
+    'dark', 
+    'light',
+    'data-theme-dark',
+    'data-theme-light',
+    // ajoute toutes les classes dynamiques nécessaires
+  ],
   theme: {
     extend: {
       colors: {
@@ -15,8 +22,10 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
+  
   daisyui: {
     themes: ["light", "dark"], // tu peux aussi mettre "cupcake", "bumblebee", etc.
+    darkTheme: "dark",         // thème utilisé quand `dark` est activé
     base: true,
     styled: true,
     utils: true,
